cb695ccfca670883661d34f1d44e64d3b6e8fbf8
SkynetSystemsRD <skynetsystemsrd@gmail.com>
2025-02-07 21:28:59 -0400

    modified:   src/layouts/components/DefaultLayoutWithHorizontalNav.vue

--- src/layouts/components/DefaultLayoutWithHorizontalNav.vue
+++ src/layouts/components/DefaultLayoutWithHorizontalNav.vue
@@ -1,19 +1,12 @@
 <script lang="ts" setup>
-import navItems from '@/navigation/horizontal'
+import navItems from '@/navigation/horizontal';
 
-import { themeConfig } from '@themeConfig'
+import { themeConfig } from '@themeConfig';
 
 // Components
-import Footer from '@/layouts/components/Footer.vue'
-import NavBarNotifications from '@/layouts/components/NavBarNotifications.vue'
-import NavSearchBar from '@/layouts/components/NavSearchBar.vue'
-import NavbarShortcuts from '@/layouts/components/NavbarShortcuts.vue'
-import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
-import UserProfile from '@/layouts/components/UserProfile.vue'
-import NavBarI18n from '@core/components/I18n.vue'
-import { HorizontalNavLayout } from '@layouts'
+import Footer from '@/layouts/components/Footer.vue';
+import { HorizontalNavLayout } from '@layouts';
 import { VNodeRenderer } from '@layouts/components/VNodeRenderer'; /* PartiallyEnd: #3632/scriptSetup.vue */
-import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
 </script>
 
 <template>
@@ -32,7 +25,7 @@ import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
       </RouterLink>
       <VSpacer />
 
-      <NavSearchBar trigger-btn-class="ms-lg-n3" />
+      <!-- <NavSearchBar trigger-btn-class="ms-lg-n3" />
 
       <NavBarI18n
         v-if="themeConfig.app.i18n.enable && themeConfig.app.i18n.langConfig?.length"
@@ -42,7 +35,7 @@ import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
       <NavbarThemeSwitcher />
       <NavbarShortcuts />
       <NavBarNotifications class="me-2" />
-      <UserProfile />
+      <UserProfile /> -->
     </template>
 
     <!-- 👉 Pages -->
