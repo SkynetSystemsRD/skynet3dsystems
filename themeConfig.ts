a62a81bb381cef3c171a81b28053d62fa6dd8d5d
SkynetSystemsRD <skynetsystemsrd@gmail.com>
2025-02-09 09:03:51 -0400

    modified:   themeConfig.ts

--- themeConfig.ts
+++ themeConfig.ts
@@ -10,7 +10,7 @@ import { AppContentLayoutNav, ContentWidth, FooterType, NavbarType } from '@layo
 
 export const { themeConfig, layoutConfig } = defineThemeConfig({
   app: {
-    title: 'skynet 3D systemss',
+    title: 'skynet 3D systems',
     logo: h('div', { innerHTML: logo, style: 'line-height:0; color: rgb(var(--v-global-theme-primary))' }),
     contentWidth: ContentWidth.Boxed,
     contentLayoutNav: AppContentLayoutNav.Vertical,
