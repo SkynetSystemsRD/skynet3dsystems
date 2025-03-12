b20c824cb073dadd5d1b454f7693bc2173753c6c
SkynetSystemsRD <skynetsystemsrd@gmail.com>
2025-03-12 13:25:38 -0400

    modified:   src/layouts/components/UserProfile.vue

--- src/layouts/components/UserProfile.vue
+++ src/layouts/components/UserProfile.vue
@@ -31,7 +31,7 @@ const userProfileList = [
   { type: 'navItem', icon: 'tabler-user', title: 'Perfil', to: { name: 'apps-user-view-id', params: { id: userData.id } } },
   { type: 'navItem', icon: 'tabler-settings', title: 'configuracion', to: { name: 'pages-account-settings-tab', params: { tab: 'account' } } },
   { type: 'divider' },
-  { type: 'navItem', icon: 'tabler-book-2', title: 'Mis Projectos', to: { name: 'pages-pricing' } },
+  { type: 'navItem', icon: 'tabler-book-2', title: 'Mis Projectos', to: { name: 'apps-all-projects-project-details' } },
   // { type: 'navItem', icon: 'tabler-question-mark', title: 'FAQ', to: { name: 'pages-faq' } },
 ]
 </script>
