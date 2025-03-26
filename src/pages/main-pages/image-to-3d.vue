edbdc508b81e0ecdc6d162fff95c09d8b13e6c01
SkynetSystemsRD <skynetsystemsrd@gmail.com>
2025-03-26 18:06:45 -0400

    modified:   src/pages/main-pages/image-to-3d.vue

--- src/pages/main-pages/image-to-3d.vue
+++ src/pages/main-pages/image-to-3d.vue
@@ -557,6 +557,7 @@ const quote = async () => {
     return;
 
   saveData('octetData', `data:application/octet-stream;base64,${fileModelContent.value}`);
+  saveData('imageData', `data:image/png;base64,${fileModelContent.value}`);
 
   await router.push({
     path: '/main-pages/model-checkout',
